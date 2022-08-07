5e41beb5ad4fdc136d921676dee3d97bd4ace523
Paul Lymburner <30843471+plymburner@users.noreply.github.com>
2022-08-06 23:42:55 -0400

    Intermediate Update Commitments
    updates and additions

--- queries.sql
+++ queries.sql
@@ -6,3 +6,63 @@ SELECT
 FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
 
+SELECT 
+    COUNT(*)
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT 
+    first_name, 
+    last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT 
+	first_name, 
+	last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+ -- Retirement eligibility
+SELECT 
+	COUNT(*)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');   
+
+--Retirement Eligible Employees Table
+SELECT 
+    first_name, 
+    last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+    AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * 
+FROM retirement_info;    
+
+-- Create new table for retiring employees
+SELECT 
+	emp_no, 
+	first_name, 
+	last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT 
+	departments.dept_name,
+    dept_manager.emp_no,
+    dept_manager.from_date,
+    dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+	ON departments.dept_no = dept_manager.dept_no;
--- schema.sql
+++ schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE employees (
 
 --Department Manager
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+dept_no VARCHAR(5) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -40,3 +40,22 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+--Titles
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title varchar NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+--dept_emp
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no varchar NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (dept_no, emp_no)
+);
