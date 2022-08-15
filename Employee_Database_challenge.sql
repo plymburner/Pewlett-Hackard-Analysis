0f4e40632f1b210e6ff6545ea145c0ab30d5fc57
Paul Lymburner <30843471+plymburner@users.noreply.github.com>
2022-08-15 00:04:34 -0400

    Final Commitment

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -33,3 +33,57 @@ ORDER BY
 --Validate only to_date of '9999-01-01' exists
 SELECT distinct to_date
 FROM current_emp_title;
+
+--Last Title of Every Employee
+SELECT DISTINCT ON (t.emp_no)
+	t.emp_no,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO last_emp_title	
+FROM titles t
+ORDER BY
+	t.emp_no,
+	t.to_date DESC;
+
+--Visual Check of the data
+SELECT *
+FROM last_emp_title;
+
+--To Create the table with Employee Titles Export
+SELECT 
+	r.emp_no,
+	r.first_name,
+	r.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM retirement_info r
+INNER JOIN titles t 
+	ON r.emp_no = t.emp_no
+ORDER BY emp_no	
+
+--Find the most recent Title
+SELECT DISTINCT ON (r.emp_no)
+	r.emp_no,
+	r.first_name,
+	r.last_name,
+	r.title,
+	r.from_date,
+	r.to_date
+FROM retirement_titles r
+--WHERE to_date = '9999-01-01'
+ORDER BY emp_no	
+
+--Most Recent Title for current employees
+SELECT DISTINCT ON (r.emp_no)
+	r.emp_no,
+	r.first_name,
+	r.last_name,
+	r.title,
+	r.from_date,
+	r.to_date
+FROM retirement_titles r
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no	
--- queries.sql
+++ queries.sql
@@ -176,4 +176,123 @@ INNER JOIN departments AS d
 	ON (de.dept_no = d.dept_no);
 	
 SELECT * FROM dept_info	
-    
+
+--Test the number of overall records
+SELECT COUNT(*)
+FROM employees
+ 
+ --Added columns for checking the data
+ SELECT --DISTINCT ON (t.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	t.title,
+	t.from_date,
+	t.to_date,
+	CASE WHEN birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+		THEN 1
+		ELSE 0
+		END AS retirement_age
+INTO retirement_titles
+FROM employees e
+LEFT JOIN titles t 
+	ON e.emp_no = t.emp_no
+--WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY
+	t.emp_no,
+	t.to_date DESC
+--LIMIT 100
+
+--Double check the count versus the employee table
+SELECT  COUNT(DISTINCT emp_no)
+FROM retirement_titles	
+
+--Query for the retirement_titles.csv extract
+SELECT 
+	emp_no,
+	first_name,
+	last_name,
+	title,
+	from_date,
+	to_date
+FROM retirement_titles
+WHERE retirement_age = 1
+
+--Unique titles table insert
+SELECT DISTINCT ON (emp_no)
+	emp_no,
+	first_name,
+	last_name,
+	title,
+	to_date
+INTO unique_titles	
+FROM retirement_titles 
+WHERE retirement_age = 1
+	AND to_date = '9999-01-01'
+ORDER BY 
+	emp_no
+
+--Unique titles extract 
+SELECT
+	emp_no,
+	first_name,
+	last_name,
+	title
+FROM unique_titles
+
+--Checkpoint Query
+SELECT
+	title,
+	COUNT(emp_no)
+FROM unique_titles
+GROUP BY
+	title
+ORDER BY
+	COUNT(emp_no) DESC
+
+--Create retiring_titles table
+SELECT
+	COUNT(emp_no),
+	title
+INTO retiring_titles	
+FROM unique_titles
+GROUP BY
+	title
+ORDER BY
+	COUNT(emp_no) DESC
+	
+--intermediate employee mentorship processing table
+SELECT
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	t.title,
+	t.from_date,
+	t.to_date,
+	CASE WHEN birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+		THEN 1
+		ELSE 0
+		END AS mentorship_age
+INTO mentorship_process
+FROM employees e
+LEFT JOIN titles t 
+	ON e.emp_no = t.emp_no
+ORDER BY
+	t.emp_no,
+	t.to_date DESC	
+
+--Final mentorship_eligibility table
+SELECT DISTINCT ON (emp_no)
+	emp_no,
+	first_name,
+	last_name,
+	birth_date,
+	from_date,
+	to_date,
+	title
+INTO mentorship_eligibilty
+FROM mentorship_process
+WHERE to_date = '9999-01-01'
+	AND mentorship_age = 1	
